ba60121c33132bb73fc31a5488e7e8bf50bc93ca
lee-janice <janicel375@gmail.com>
2026-06-08 15:39:45 -0400

    Update resume

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ const IndexPage: React.FC<PageProps> = () => {
                         Pomona College
                     </a>{" "}
                     in 2021. Click{" "}
-                    <a href="/20251023_lee_janice_resume.pdf" target="_blank">
+                    <a href="/20260605_lee_janice_resume.pdf" target="_blank">
                         here
                     </a>{" "}
                     for my full resume.
